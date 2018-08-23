eaa9dc7b8ecc647cdf6dce22e9b738a8a9c924ff
Victoria Luc <victoriajeniluc@gmail.com>
2018-08-23 14:35:07 -0500

    Initial commit

---
+++ src/CommandLine1.java
@@ -0,0 +1,9 @@
+public class CommandLine1 {
+    public static void main(String[] args) {
+        String [] arr = new String[]{"John", "Joe", "Lucy"};
+
+        for(String val : arr) {
+            System.out.println(val);
+        }
+    }
+}
